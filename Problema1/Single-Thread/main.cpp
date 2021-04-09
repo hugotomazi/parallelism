3772e14b8ff2e00f1a36d1a2c1614fadd111ba91
Hugo Tomazi Busnardo <hugo.busnardo@gmail.com>
2021-04-09 20:32:29 -0300

    (feat) Implementado problema 1 no modo Single Thread

--- Problema1/Single-Thread/main.cpp
+++ Problema1/Single-Thread/main.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
+#include <random>
 #include <time.h>
 
 using namespace std;
 
-#define LINHAS 100
-#define COLUNAS 500
+#define LINHAS 5000
+#define COLUNAS 5000
 
 int m1[LINHAS][COLUNAS];
 int m2[COLUNAS][LINHAS];
 int resultado[LINHAS][COLUNAS];
 
-int main(int argc, char *argv[]) {
+void initMatrix() {
+    random_device dev;
+    mt19937 rng(dev());
+    uniform_int_distribution<mt19937::result_type> dist6(0,LINHAS*COLUNAS);
+    for(int i = 0; i < LINHAS; ++i) {
+        for(int j = 0; j < COLUNAS; ++j) {
+            m1[i][j] = dist6(rng);
+            m2[j][i] = dist6(rng);
+        }
+    }
+}
 
+int main(int argc, char *argv[]) {
+    double inicio, fim;
+    initMatrix();
+
+    inicio = (double) clock() / CLOCKS_PER_SEC;
+    for(int i = 0; i < LINHAS; ++i) {
+        for(int j = 0; j < COLUNAS; ++j) {
+            resultado[i][j] = m1[i][j] * m2[j][i];
+        }
+    }
+
+    fim = (double) clock() / CLOCKS_PER_SEC;
+
+    cout << "Tempo de execução: " << (fim - inicio) << endl;
     return 0;
 }
